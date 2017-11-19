b652b882467e2a3f80a5f1e6916ce74208c929f0
Maaz Muzammil <maazmuzammil.1426@gmail.com>
2017-11-19 15:28:23 +0530

    Update and rename lineage.mk to dot.mk

--- dot.mk
+++ dot.mk
@@ -15,12 +15,12 @@
 $(call inherit-product, device/yu/jalebi/full_jalebi.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
 
-PRODUCT_NAME := lineage_jalebi
+PRODUCT_NAME := dot_jalebi
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := jalebi
 
@@ -29,3 +29,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-micromax
 TARGET_VENDOR_PRODUCT_NAME := YUNIQUE
 TARGET_VENDOR_DEVICE_NAME := YUNIQUE
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=YUNIQUE PRODUCT_NAME=YUNIQUE
+DOT_OFFICIAL := true
+ROOT_METHOD := magisk
