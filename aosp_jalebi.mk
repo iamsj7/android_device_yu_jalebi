c8a6d2d662dddcd3f16bc77ea27e9848b02a5494
iamsj7 <shaikjaleel270@gmail.com>
2018-02-18 14:44:32 +0000

    Lets AEx-iFy

--- aosp_jalebi.mk
+++ aosp_jalebi.mk
@@ -16,14 +16,16 @@
 $(call inherit-product, device/yu/jalebi/full_jalebi.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
 
-PRODUCT_NAME := lineage_jalebi
+PRODUCT_NAME := aosp_jalebi
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := jalebi
+TARGET_BOOT_ANIMATION_RES := 720
+EXTENDED_BUILD_TYPE := OFFICIAL
 
 PRODUCT_GMS_CLIENTID_BASE := android-micromax
 
