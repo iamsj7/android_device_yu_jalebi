fb24cee197cb87df381c1942fa7a5b25e1c6943b
Ad!thya R <adithya.r02@outlook.com>
2018-05-10 11:58:55 +0530

    jalebi: camera: Switch to camera device 3.2

--- device.mk
+++ device.mk
@@ -49,7 +49,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl
+    camera.device@3.2-impl
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
