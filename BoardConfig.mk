f3df167cb0e942b4e175cb8870a16089a3e6cebe
iamsj7 <shaikjaleel270@gmail.com>
2018-05-10 10:42:13 +0000

    jalebi: fix no volume on volte calls

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,6 +22,9 @@ include device/cyanogen/msm8916-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/yu/jalebi
 
+# Audio
+AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
+
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 
