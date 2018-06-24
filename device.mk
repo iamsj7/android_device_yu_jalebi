1ecb3e3785a462a8b0adb2956dc5f669d49a8b22
Harshit Jain <harshitjain6751@gmail.com>
2018-06-24 19:17:59 +0530

    jalebi: build textclassifier's smart selection
    See -> https://source.android.com/devices/tech/display/textclassifier

--- device.mk
+++ device.mk
@@ -117,6 +117,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    textclassifier.smartselection.bundle1
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/vendor/etc/thermal-engine.conf
