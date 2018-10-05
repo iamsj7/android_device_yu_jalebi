4f8a13dea8c9d274b7e9fec31be865d23f9fa616
Shaik Jaleel <shaikjaleel270@gmail.com>
2018-10-05 12:32:20 +0000

    jalebi: USE GCC LINARO TOOLCHAIN
    * USE TOOLCHAIN https://github.com/dev-harsh1998/GCC_BUILDS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,7 +56,7 @@ BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
 TARGET_KERNEL_CONFIG := lineageos_jalebi_defconfig
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-7.2/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/linaro/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 
 # Lights
