e34bf93f8285b544d0074e94a426cb72df7d0e72
Shaik Jaleel <shaikjaleel270@gmail.com>
2018-12-14 10:16:11 +0000

    jalebi: tune perf && set lcd density to 280

--- device.mk
+++ device.mk
@@ -51,12 +51,25 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@3.2-impl
 
-# Dalvik
+# Optimize
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=128m \
     dalvik.vm.heapminfree=6m \
-    dalvik.vm.heapstartsize=14m
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapsize=256m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapmaxfree=8m \
+    dalvik.vm.zygotemaxfailedboots=5 \
+    dalvik.vm.foreground-heap-growth-multiplier=2.0 \
+    dalvik.vm.dex2oat-flags=--no-watch-dog \
+    dalvik.vm.dex2oat-swap=false \
+    dalvik.vm.dex2oat-threads=2 \
+    ro.vendor.qti.am.reschedule_service=true \
+    sys.use_fifo_ui=1
 
 # Display
 PRODUCT_PACKAGES += \
